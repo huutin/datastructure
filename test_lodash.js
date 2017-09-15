11df78cb728d699fc203ca15694714143d0511fe
Tin Dang <danghuutin@gogojungle.vn>
2017-09-16 02:50:13 +0700

    uu

--- test_lodash.js
+++ test_lodash.js
@@ -1,17 +1,13 @@
 var _ = require('lodash');
-var users = [
-    { 'user': 'barney', 'age': 36 },
-    { 'user': 'fred', 'age': 40 },
-    { 'user': 'pebbles', 'age': 1 }
-];
 
-var youngest = _
-    .chain(users)
-    .sortBy('age')
-    .map(function(o) {
-        return o.user + ' is ' + o.age;
-    })
-    .head()
-    .value();
-console.log(youngest);
-console.log(users);
+const normalize = (str) => str.replace(/\-/g, '');
+const validLength = (param, str) => str.length === param;
+const checkLengthSsn =  _.partial(validLength, 9);
+const cleanInput = (str) => {
+  return _.curry(normalize)(_.trim(str))
+}
+const isValidSsn = (str) => {
+  return checkLengthSsn(cleanInput(str))
+}
+console.log(cleanInput(' 444-44-4444 '))
+console.log(isValidSsn(' 444-44-4444 '))

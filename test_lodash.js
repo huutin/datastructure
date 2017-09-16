c057a963a419b8aa66a8874b504abfc884c83c5f
Tin Dang <danghuutin@gogojungle.vn>
2017-09-16 19:02:22 +0700

    uu

--- test_lodash.js
+++ test_lodash.js
@@ -1,13 +1,10 @@
 var _ = require('lodash');
-
 const normalize = (str) => str.replace(/\-/g, '');
 const validLength = (param, str) => str.length === param;
-const checkLengthSsn =  _.partial(validLength, 9);
-const cleanInput = (str) => {
-  return _.curry(normalize)(_.trim(str))
-}
-const isValidSsn = (str) => {
-  return checkLengthSsn(cleanInput(str))
-}
-console.log(cleanInput(' 444-44-4444 '))
-console.log(isValidSsn(' 444-44-4444 '))
+const checkLengthSsn = _.partial(validLength, 9);
+const cleanInput = _.flow(_.trim, normalize)
+const isValidSsn = _.flow(cleanInput, checkLengthSsn)
+const myStr = ' 444-44-4444 '
+console.log(cleanInput(myStr))
+console.log(isValidSsn(myStr))
+console.log(myStr)

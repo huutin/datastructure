f572135af131cd33125e7bd08d96b00490930555
Tin Dang <danghuutin@gogojungle.vn>
2017-09-26 19:03:28 +0700

    update

--- test_wrapper.js
+++ test_wrapper.js
@@ -1,31 +1,11 @@
 const _ = require('lodash');
-const Maybe = require('monet');
-
-class Wrapper {
-  constructor(value) {
-    this.value = value;
+const countOdd = (arr, i = 0) => {
+  if (arr[i] === undefined) {
+    return 0
   }
-  map(f) {
-    return f(this.value);
-  };
-  toString() {
-    return 'Wrapper (' + this.value + ')';
-  }
+  return (arr[i] % 2) + countOdd(arr, ++i)
 }
-const wrap = (val) => new Wrapper(val);
-Wrapper.prototype.fmap = function (f) {
-  return wrap(f(this.value));
-};
-
-const plus = _.curry((a, b) => a + b);
-const plus3 = plus(3)
-const plus10 = plus(10)
-
-const two = wrap(2)
-const five = two.fmap(plus3)
-// console.log(two.map(_.identity))
-// console.log(five.fmap(plus10).map(_.identity))
-const test = wrap()
-// console.log(wrap().map(_.identity))
-var maybe = Maybe.Some(123);
-console.log(maybe)
+const countEven = (arr) => {
+  return arr.length - countOdd(arr)
+}
+console.log(countEven([1, 4, 5, 9, 0, 5]))

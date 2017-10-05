76128f762ef16a2257f4b647fb58b38e6d35e8fe
Tin Dang <danghuutin@gogojungle.vn>
2017-10-05 19:11:56 +0700

    u

--- test_monads.js
+++ test_monads.js
@@ -1,16 +1,13 @@
-const _ = require('lodash');
+const _ = require("lodash");
 const Maybe = require("lodash-fantasy/data/Maybe");
-const isEven = (x) => x % 2 === 0;
-const times = [
-  Array.from(Array(8).keys()).map((x) => { return (x + (x > 1 ? ' Hours' : ' Hour')) }),
-  Array.from(Array(6).keys()).map((x) => { return (x * 10 + (x > 1 ? ' Mins' : ' Min')) })
-]
-const getTimeText = (_times, x, y) => _times[x][y];
-const getHourText = _.partial(getTimeText, times, 0);
-const getMinText = _.partial(getTimeText, times, 1);
-const arr = [1, 2].reduce((result, value) => {
-  let fn = (!result.length) ? getHourText : getMinText;
-  result += Maybe.ofNullable(value).map(fn).orElse('') + " ";
-  return result
-}, "")
-console.log(arr)
+const axios = require("axios");
+axios.get('http://demo3521206.mockable.io/test').then((response) => {
+  if (response.data.code) {
+    throw response.data.code
+  }
+  return response.data
+}).then((data) => {
+  console.log(data)
+}).catch((error) => {
+  console.log(error)
+})
